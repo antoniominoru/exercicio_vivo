c3281763344bad6057d2513ca5dd44cc2d52eb7a
root <root@DESKTOP-OV2Q6EA>
2023-02-08 13:23:40 -0300

    make class

--- app/app.js
+++ app/app.js
@@ -1,11 +1,39 @@
+class Component{
+    constructor(id){
+        this.id = id;
+    }
+
+    getPercentSignal(){
+        return '%';
+    }
+}
+
+class Type extends Component{
+    constructor(id, per){
+        super(id);
+        this._id = id;
+        this.per = per;
+    }
+    getPercentChocolate() {
+        return this.per + super.getPercentSignal();
+      }
+}
+
+
+
 function percentChocolate(value){
     try{
-        if (value.toUpperCase() === 'A')
-            return ('80%')
-        else if (value.toUpperCase() === 'B')
-            return ('85%')
-        else if (value.toUpperCase() === 'C')
-            return ('90%')
+        const firstType = new Type('A', '80');
+        const secondType = new Type('B', '85');
+        const thirdType = new Type('C', '90');
+
+
+        if (value.toUpperCase() === firstType.id)
+            return (firstType.getPercentChocolate())
+        else if (value.toUpperCase() === secondType.id)
+            return (secondType.getPercentChocolate())
+        else if (value.toUpperCase() === thirdType.id)
+            return (thirdType.getPercentChocolate())
         else
             return ('Error')
     }
